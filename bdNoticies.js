388b5f01ed1b892c388145b33cdedac4447c0bef
ArnauAgudo <159519789+ArnauAgudo@users.noreply.github.com>
2025-03-22 18:58:38 +0000

    commit

---
+++ bdNoticies.js
@@ -0,0 +1,38 @@
+const bd = [
+    {
+        id: 1,
+        titol: "Nova tecnologia revoluciona el sector energètic",
+        descripcio: "Investigadors han desenvolupat una nova font d'energia sostenible...",
+        imatge: "img/noticia1.jpg",
+        data: "2025-03-22"
+    },
+    {
+        id: 2,
+        titol: "Descoberta una exoplaneta habitable",
+        descripcio: "Astrònoms han identificat un nou planeta amb condicions similars a la Terra...",
+        imatge: "img/noticia2.jpg",
+        data: "2025-03-21"
+    },
+    {
+        id: 3,
+        titol: "Innovacions en la medicina regenerativa",
+        descripcio: "Nous avenços permeten regenerar teixits amb biotecnologia avançada...",
+        imatge: "img/noticia3.jpg",
+        data: "2025-03-20"
+    },
+    {
+        id: 4,
+        titol: "Intel·ligència artificial en l'educació",
+        descripcio: "Les noves eines d'IA estan canviant la forma d'ensenyar i aprendre...",
+        imatge: "img/noticia4.jpg",
+        data: "2025-03-19"
+    },
+    {
+        id: 5,
+        titol: "El canvi climàtic i les seves conseqüències",
+        descripcio: "Experts alerten sobre els efectes cada vegada més extrems del clima...",
+        imatge: "img/noticia5.jpg",
+        data: "2025-03-18"
+    }
+];
+
